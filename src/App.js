aa102a6ad6b9e2c66b3f84ae391af855215bf870
Hyeonseo Jo <jhs2468@naver.com>
2023-10-20 17:43:00 +0900

    231020

--- src/App.js
+++ src/App.js
@@ -1,48 +1,162 @@
-import logo from './logo.svg';
+import { v4 as uuid } from "uuid";
 import './App.css';
-import {useState} from "react";
+import { useState } from "react";
 
+// camelcase -> 띄어쓰기가 필요한 곳에 대문자로 표시한다
+// ex) background-color -> backgroundColor
+//     font-size -> fontSize
 function App() {
   // react way
-  const [todos, setTodos] = useState(["할일 1", "할일 2"]);
-  // stat 변화하는 값
-  // React -> state가 변할 때마다 화면을 다시 그린다.
-  // ["할일 1", "할일 2"];
+  // state는 새로운 값으로 대체된다
+  const [inputValue, setInputValue] = useState("");
+  /**
+   * 세트메뉴
+   * {
+   *  id: 중복되지 않는 id값
+   *  name: 메뉴 이름
+   *  price: 가격
+   *  hamburger: 햄버거 종류
+   *  isPotato: 감자튀김을 받을지 말지
+   *  soda: 음료수 종류
+   * }
+   */
+  /**
+  * TODO object, 객체
+  * {
+  * id: 중복되지 않는 id값, uuid 항상 고유한 키값이 나오는 암호화 로직을 갖고 있는 라이브러리 // npm i uuid
+  * content: 할 일에 대해 적은 내용 string ('할일 1')
+  * createAt: 생성된 시간 number (0~9128439184921490)
+  * idDone: 완료 여부 (true, false)
+  * }
+   */
 
+  const [todos, setTodos] = useState([]);
+  const [sort, setSort] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const computedTodos = todos
+  .filter((todo) => {
+    if (filter === 'ALL') return true;
+    if (filter === 'DONE') return todo.isDone === true;
+    if (filter === "NOT DONE") return todo.isDone === false; // !todo.isDone
+  })
+  .sort((a, b) => {
+    if (sort === 'none') return 0; // 0이 의미하는 것 바꾸지 않겠다
+    if (sort === 'createAt') return b.createAt - a.createAt;
+    if (sort === 'content') return a.content.localeCompare(b.content);
+  });
+
+  // console.log({ sort });
+  // state 변화하는 값, 임시적인 값
+  // React -> state가 변할때마다 화면을 다시 그린다.
+  // ["할일 1", "할일 2"] -> ["할일 1", "할일 2", "할일 3"]];
 
   return (
     // JSX (JS -> HTML)
     <div className="App">
       <h1>TODO LIST</h1>
       <div>
-        <input />
-        <button>ADD</button>
+        <label>필터 : </label>
+        <input
+          type="radio"
+          value="ALL"
+          checked={filter === "ALL"}
+          onChange={(e) => setFilter(e.target.value)} 
+        />
+        <label>전체</label>
+        <input 
+          type="radio"
+          value="DONE"
+          checked={filter === "DONE"}
+          onChange={(e) => setFilter(e.target.value)} 
+        />
+        <label>완료</label>
+        <input type="radio"
+          value="NOT DONE"
+          checked={filter === "DNOT DONEONE"}
+          onChange={(e) => setFilter(e.target.value)} 
+        />
+        <label>미완료</label>
       </div>
-      {/* {DRY Don't Repeat Yourself} */}
-      {todos.map((todo, index) => (
-        <div key={index}>
-          <input type="checkbox" />
-          <span>{todo}</span>
-          <button>DEL</button>
-        </div>
-      ))}
+      <div>
+        <label htmlFor="sort">정렬 : </label>
+        <select value={sort} onChange={(e) => setSort(e.target.value)}>
+          <option value="none">생성순</option>
+          <option value="createAt">최신순</option>
+          <option value="content">가나다순</option>
+        </select>
+      </div>
+
+      <div>
+        {/* (인자) => {어떤 행동} */}
+        <input
+          // Input의 제어권을 React(JS)가 가지고 있을 수 있게, state값을 주입했다. 
+          value={inputValue}
+          // Input의 값이 변하는 이벤트가 발생했을 때, 제어권ㅇ르 가진 React(JS)의 state값을 변경한다.
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            // spread 연산자
+            const newTodo = {
+              id: uuid(),
+              content: inputValue,
+              isDone: false,
+              createAt: Date.now(),
+            };
+            setTodos([...todos, newTodo]); // '...' 기존 값을 그대로 이용할거야
+            setInputValue("");
+          }}
+        >
+          ADD
+        </button>
+      </div>
+      <div>
+        {/* DRY Don't Repeat Yourself */}
+        {/* [할일 1, 할일 2, 할일 3]  */}
+        {computedTodos.map((todo, index) => (
+          <div key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.isDone}
+              onChange={(e) => {
+                /**
+                 * todos :
+                 * { content : "할일 1"
+                 * createdAt : 1697782607507
+                 * id : "38b5c80a-008a-47f4-81f1-bb50fa206c29"
+                 * isDone : false },
+                 * 두번째 todo,
+                 * 세번째 todo
+                 */
+                // map하는 것은 처음부터 끝까지 보는 것
+                const nextTodos = todos.map((todo, idx) =>
+                  idx === index ? { ...todo, isDone: e.target.checked } : todo
+                );
+                setTodos(nextTodos);
+              }}
+            />
+            <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>
+              {todo.content}
+            </span>
+            <button
+              onClick={() => {
+                const nextTodos = todos.filter((_, idx) => idx !== index);
+                setTodos(nextTodos);
+              }}
+            >
+              DEL
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default App;
 
-{/* <input
-        style={{
-          border: "solid 1px green",
-          borderRadius: "20px",
-          border: "solid 1px green",
-          borderRadius: "20px",
-          outline: "none",
-        }}
-      /> */}
-// onChange={(e) => {
-//   if (e.target.value.includes("바보")) {
-//     alert("바보라는 글자는 사용할 수 없습니다.");
-//   }
-// }}
+// 내용 수정(update 할 수 있는 동작), 우선 순위, 디자인 +? 쓰레기통 아이콘?
+// 심화 - 새로 고침 했을때 내용이 그대로 남아있게 하기
